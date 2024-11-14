d8f241a1163a6aaeaac594e032c2233a8dedcf34
Andrew <andrewlang.p@gmail.com>
2024-11-13 23:44:52 -0500

    create node postgres to connect frontend to database and test queries

---
+++ bootcamp_app/students.js
@@ -0,0 +1,28 @@
+// psql -h localhost -p 5432 -U development bootcampx
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'development',
+  password: 'development',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const args = process.argv.slice(2);
+
+pool
+  .query(
+    `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE '%${args[0]}%'
+LIMIT ${args[1] || 5};
+`
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`)
+    })
+  })
+  .catch((err) => console.error("query error", err.stack));
